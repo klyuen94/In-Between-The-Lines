d0bfcf6bb53f5e0596e73de098e52c38e9e47a7e
klyuen94 <klyuen@uci.edu>
2017-06-02 14:52:16 -0700

    Visual regression classifier learner

---
+++ train_classifier.py
@@ -0,0 +1,36 @@
+
+import cv2
+import numpy as np
+from sklearn.linear_model import SGDClassifier
+import csv
+video_height = 240
+video_width = 432
+
+def train_classifier():
+    #creates an np array to store all the features for each fram collected from the oracle
+    filename = "C:/Users/hrvega1510/Desktop/CS175Dataset/images/"
+    count = 0
+    lr = SGDClassifier()
+    #populates the x_train np array with edge detection data for each fram collected
+    with open('actions.csv', 'rb') as f:
+        reader = csv.reader(f)
+        for rows in reader:
+            y_data = list(rows)
+            print y_data
+            size = len(y_data)
+            y_train = np.array(y_data)
+            img = cv2.imread(filename + str(0) + ".png")
+            edges = cv2.Canny(img, 200, 300)
+            x_train = edges.flatten()
+            for i in range(count*(size),size*(count+1)-1):
+                img = cv2.imread(filename+str(i)+".png")
+                edges = cv2.Canny(img, 200, 300).flatten()
+                x_train = np.vstack((x_train,edges))
+            count = count + 1
+            print "training " + str(count)
+            lr.fit(x_train, y_train)
+    f.close()
+    return lr
+
+
+
