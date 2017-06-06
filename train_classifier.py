6256851d87b04cc9747e8b4bf3d451f236b5fbfb
klyuen94 <klyuen@uci.edu>
2017-06-05 23:34:20 -0700

    uses neural network now

--- train_classifier.py
+++ train_classifier.py
@@ -2,6 +2,7 @@
 import cv2
 import numpy as np
 from sklearn.linear_model import SGDClassifier
+from sklearn.neural_network import MLPClassifier
 import csv
 video_height = 240
 video_width = 432
@@ -10,8 +11,8 @@ def train_classifier():
     #creates an np array to store all the features for each fram collected from the oracle
     filename = "C:/Users/hrvega1510/Desktop/CS175Dataset/images/"
     count = 0
-    lr = SGDClassifier()
-    #populates the x_train np array with edge detection data for each fram collected
+    lr =  MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+    #populates the x_train np array with edge detection data for each frame collected
     with open('actions.csv', 'rb') as f:
         reader = csv.reader(f)
         for rows in reader:
